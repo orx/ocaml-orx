10613e03e57a14f293d4f550e208ee94e4901ee3
Hezekiah M. Carty <hez@0ok.org>
2020-05-01 16:26:20 -0600

    Update beginner's guide

--- examples/wiki/beginners_guide/beginners_guide.ml
+++ examples/wiki/beginners_guide/beginners_guide.ml
@@ -4,26 +4,14 @@
    This tries to follow the guide fairly closely, hopefully making it easy to
    see how the Orx API calls used here match their C counterparts. *)
 
-(* Helper functions for unwrapping values. Proper error handling would be *)
-(* ideal in a full game implementation but would obscure the intent in this *)
-(* translation of the beginner's guide. *)
-let get_ok (r : ('a, _) result) : 'a =
-  match r with
-  | Ok x -> x
-  | Error _ -> invalid_arg "get_ok: argument must be Ok(_)"
-let get_some (o : 'a option) : 'a =
-  match o with
-  | Some x -> x
-  | None -> invalid_arg "get_some: argument must be Some(_)"
-
 module Helpers = struct
   (* Create an explosion named name (defined in the game's configuration) at *)
   (* the object o *)
   let create_explosion_at_object (o : Orx.Object.t) (name : string) =
-    let position = Orx.Object.get_world_position o |> get_some in
+    let position = Orx.Object.get_world_position o |> Option.get in
     Orx.Vector.set_z position 0.0;
-    let explosion = Orx.Object.create_from_config name |> get_some in
-    Orx.Object.set_position explosion position |> get_ok
+    let explosion = Orx.Object.create_from_config name |> Option.get in
+    Orx.Object.set_position explosion position |> Result.get_ok
 end
 
 (* Global game state *)
@@ -40,13 +28,14 @@ module State = struct
 
   let state : t option ref = ref None
 
-  let get () = get_some !state
+  let get () = Option.get !state
 
   (* Increase the score by a given amount *)
   let increase_score (state : t) (earned : int) : unit =
     state.score <- state.score + earned;
     let formatted_score = Fmt.strf "%06d" state.score in
-    Orx.Object.set_text_string state.score_object formatted_score |> get_ok
+    Orx.Object.set_text_string state.score_object formatted_score
+    |> Result.get_ok
 end
 
 module Physics = struct
@@ -60,24 +49,24 @@ module Physics = struct
     let recipient_name = Orx.Object.get_name recipient in
 
     if String.equal sender_name "StarObject" then (
-      Orx.Object.set_life_time sender 0.0 |> get_ok;
+      Orx.Object.set_life_time sender 0.0 |> Result.get_ok;
       State.increase_score state 1000
     );
     if String.equal recipient_name "StarObject" then (
-      Orx.Object.set_life_time recipient 0.0 |> get_ok;
+      Orx.Object.set_life_time recipient 0.0 |> Result.get_ok;
       State.increase_score state 1000
     );
 
     if String.equal sender_name "BulletObject" then (
       Helpers.create_explosion_at_object recipient "JellyExploder";
-      Orx.Object.set_life_time sender 0.0 |> get_ok;
-      Orx.Object.set_life_time recipient 0.0 |> get_ok;
+      Orx.Object.set_life_time sender 0.0 |> Result.get_ok;
+      Orx.Object.set_life_time recipient 0.0 |> Result.get_ok;
       State.increase_score state 250
     );
     if String.equal recipient_name "BulletObject" then (
       Helpers.create_explosion_at_object sender "JellyExploder";
-      Orx.Object.set_life_time sender 0.0 |> get_ok;
-      Orx.Object.set_life_time recipient 0.0 |> get_ok;
+      Orx.Object.set_life_time sender 0.0 |> Result.get_ok;
+      Orx.Object.set_life_time recipient 0.0 |> Result.get_ok;
       State.increase_score state 250
     );
 
@@ -86,18 +75,20 @@ module Physics = struct
       && String.equal sender_name "MonsterObject"
     then (
       Helpers.create_explosion_at_object recipient "HeroExploder";
-      Orx.Object.set_life_time sender 0.0 |> get_ok;
+      Orx.Object.set_life_time sender 0.0 |> Result.get_ok;
       Orx.Object.enable recipient false;
-      Orx.Object.add_time_line_track state.scene "PopUpGameOverTrack" |> get_ok
+      Orx.Object.add_time_line_track state.scene "PopUpGameOverTrack"
+      |> Result.get_ok
     );
     if
       String.equal sender_name "HeroObject"
       && String.equal recipient_name "MonsterObject"
     then (
       Helpers.create_explosion_at_object sender "HeroExploder";
-      Orx.Object.set_life_time recipient 0.0 |> get_ok;
+      Orx.Object.set_life_time recipient 0.0 |> Result.get_ok;
       Orx.Object.enable sender false;
-      Orx.Object.add_time_line_track state.scene "PopUpGameOverTrack" |> get_ok
+      Orx.Object.add_time_line_track state.scene "PopUpGameOverTrack"
+      |> Result.get_ok
     );
 
     Ok ()
@@ -120,21 +111,23 @@ let bootstrap () =
 
 let init () =
   (* Get some values defined in the game's ini config *)
-  let viewport = Orx.Viewport.create_from_config "Viewport" |> get_some in
-  let hero = Orx.Object.create_from_config "HeroObject" |> get_some in
-  let heros_gun = Orx.Object.get_child_object hero |> get_some in
-  let score_object = Orx.Object.create_from_config "ScoreObject" |> get_some in
-  let scene = Orx.Object.create_from_config "Scene" |> get_some in
+  let viewport = Orx.Viewport.create_from_config "Viewport" |> Option.get in
+  let hero = Orx.Object.create_from_config "HeroObject" |> Option.get in
+  let heros_gun = Orx.Object.get_child_object hero |> Option.get in
+  let score_object =
+    Orx.Object.create_from_config "ScoreObject" |> Option.get
+  in
+  let scene = Orx.Object.create_from_config "Scene" |> Option.get in
 
   State.state :=
     Some { hero; heros_gun; viewport; score_object; scene; score = 0 };
 
-  Orx.Object.create_from_config "PlatformObject" |> get_some |> ignore;
+  Orx.Object.create_from_config "PlatformObject" |> Option.get |> ignore;
 
   (* No shooting to start out *) Orx.Object.enable heros_gun false;
 
   (* Setup our physics event handler *)
-  Orx.Event.add_handler Physics Physics.event_handler |> get_ok;
+  Orx.Event.add_handler Physics Physics.event_handler |> Result.get_ok;
 
   Ok ()
 
@@ -152,19 +145,19 @@ let run () =
 
   if Orx.Input.is_active "Quit" then
     (* Return an error to indicate that it's time to quit the engine *)
-    Error ()
+    Orx.Status.error
   else (
     (* Left/right movement *)
     if Orx.Input.is_active "GoLeft" then (
-      Orx.Object.set_scale state.hero flip_left |> get_ok;
-      Orx.Object.apply_impulse state.hero left_speed None |> get_ok;
-      Orx.Object.set_target_anim state.hero "HeroRun" |> get_ok
+      Orx.Object.set_scale state.hero flip_left |> Result.get_ok;
+      Orx.Object.apply_impulse state.hero left_speed None |> Result.get_ok;
+      Orx.Object.set_target_anim state.hero "HeroRun" |> Result.get_ok
     ) else if Orx.Input.is_active "GoRight" then (
-      Orx.Object.set_scale state.hero flip_right |> get_ok;
-      Orx.Object.apply_impulse state.hero right_speed None |> get_ok;
-      Orx.Object.set_target_anim state.hero "HeroRun" |> get_ok
+      Orx.Object.set_scale state.hero flip_right |> Result.get_ok;
+      Orx.Object.apply_impulse state.hero right_speed None |> Result.get_ok;
+      Orx.Object.set_target_anim state.hero "HeroRun" |> Result.get_ok
     ) else
-      Orx.Object.set_target_anim state.hero "HeroIdle" |> get_ok;
+      Orx.Object.set_target_anim state.hero "HeroIdle" |> Result.get_ok;
 
     (* Shooting *)
     if Orx.Input.is_active "Shoot" then
@@ -174,9 +167,10 @@ let run () =
 
     (* Jumping *)
     if Orx.Input.is_active "Jump" && Orx.Input.has_new_status "Jump" then
-      Orx.Object.apply_impulse state.hero jump_speed None |> get_ok;
+      Orx.Object.apply_impulse state.hero jump_speed None |> Result.get_ok;
 
-    (* Done! *) Ok ()
+    (* Done! *)
+    Orx.Status.ok
   )
 
 (* Orx will handle all the cleanup we need *)
@@ -185,7 +179,7 @@ let exit () = ()
 
 let () =
   (* Setup our bootstrap function *)
-  Orx.Config.set_bootstrap bootstrap |> get_ok;
+  Orx.Config.set_bootstrap bootstrap |> Result.get_ok;
   (* Set the basename for Orx - used to know which config file to read *)
-  Orx.Config.set_basename "tutorial" |> get_ok;
+  Orx.Config.set_basename "tutorial" |> Result.get_ok;
   (* Start Orx and run the main loop *) Orx.Main.execute ~init ~run ~exit ()
